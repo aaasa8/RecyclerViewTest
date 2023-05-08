df1556ab7d151c32254652298eedbbbc7a809813
aa <aaaaj123456@naver.com>
2023-05-08 12:05:48 +0900

    .

--- app/src/main/java/ddwu/com/mobile/recyclerviewtest/MyAdeter.kt
+++ app/src/main/java/ddwu/com/mobile/recyclerviewtest/MyAdeter.kt
@@ -1,5 +1,6 @@
 package ddwu.com.mobile.recyclerviewtest
 
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,9 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 class MyAdeter(val contextval:  layout: Int, val dataList) : RecyclerView.Adapter<MyAdeter.MyViewHolder>(){
 
 
-
+    val TAG = "MyAdeter"
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-
+        Log.d(TAG, "create ViewHolder")
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
